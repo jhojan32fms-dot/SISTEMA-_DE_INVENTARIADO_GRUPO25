66a873f759e3b92a2c9ad8cfadf1f069dab34190
JuanC-J <N00486838@upn.pe>
2026-06-30 21:03:26 -0500

    Conexion BD

--- SISTEMA_INVENTARIADO_FERRETERIA_SANTA_CRUZ_SRL/src/sistema_inventariado_ferreteria_santa_cruz_srl/Conexion.java
+++ SISTEMA_INVENTARIADO_FERRETERIA_SANTA_CRUZ_SRL/src/sistema_inventariado_ferreteria_santa_cruz_srl/Conexion.java
@@ -7,7 +7,7 @@ public class Conexion {
 
     private static final String URL = "jdbc:mysql://localhost:3306/ferreteria_t1";
     private static final String USUARIO = "root";
-    private static final String PASSWORD = "root";
+    private static final String PASSWORD = "Rot456.adm";
 
     public static Connection conectar() {
 
